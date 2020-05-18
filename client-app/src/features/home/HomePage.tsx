a3b28e15a59c3348bfffd0b309e84c37718067c0
LLealL <llealdegoes@gmail.com>
2020-05-17 22:04:11 -0300

    implementação do botão para cadastro no homepage

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -3,6 +3,7 @@ import { Container, Segment, Header, Button, Image } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import LoginForm from "../user/LoginForm";
+import RegisterForm from "../user/RegisterForm";
 
 const HomePage = () => {
   const rootStore = useContext(RootStoreContext);
@@ -36,6 +37,9 @@ const HomePage = () => {
             <Header as="h2" inverted content="Welcome to HospitalManager" />
             <Button onClick={() => openModal(<LoginForm />)} size="huge" inverted>
               Login
+            </Button>
+            <Button onClick={() => openModal(<RegisterForm />)} size="huge" inverted>
+              Cadastrar
             </Button>
           </Fragment>
         )}
