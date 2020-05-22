c57e69ed1cff94a88bd927c0528da6a7e5267d90
Euller Nóbrega <eullernobrega@gmail.com>
2020-05-21 22:35:20 -0300

    Update paciente.ts

---
+++ client-app/src/app/models/paciente.ts
@@ -0,0 +1,17 @@
+export interface IPaciente {
+  id: number | null;
+  nome: string;
+  email: string;
+}
+
+export class PacienteFormValues implements IPaciente {
+  id: number | null = null;
+  nome: string = "";
+  email: string = "";
+
+  constructor(init?: IPaciente) {
+    Object.assign(this, init);
+  }
+
+    
+  }
