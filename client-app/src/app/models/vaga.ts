ee6ae5103a01918238cdf2d5c64b00bfed2a186f
LLealL <llealdegoes@gmail.com>
2020-06-04 23:34:05 -0300

    correção no model de vaga, feito em conjunto com momedes e jean

--- client-app/src/app/models/vaga.ts
+++ client-app/src/app/models/vaga.ts
@@ -5,7 +5,7 @@ export interface IVaga {
   id: number | null;
   numeroQuarto: string;
   situacao: string;
-  paciente?: IPaciente;
+  paciente?: IPaciente | null;
   user: IUser | null;
 }
 
@@ -18,7 +18,7 @@ export class VagasFormValues implements IVaga {
   id = null;
   numeroQuarto = "";
   situacao = SituacaoEnum.LIVRE;
-  paciente?: IPaciente;
+  paciente?: IPaciente | null;
   user = null;
 
   constructor(init?: IVaga) {
