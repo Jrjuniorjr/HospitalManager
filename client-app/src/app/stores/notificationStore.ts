f439ff176b5e990bb6bf4f538f8785b95ae84b62
LLealL <llealdegoes@gmail.com>
2020-06-09 19:07:54 -0300

    adicionado actions em notificationStore

--- client-app/src/app/stores/notificationStore.ts
+++ client-app/src/app/stores/notificationStore.ts
@@ -4,6 +4,7 @@ import agent from "../api/agent";
 import { history } from "../..";
 import { toast } from "react-toastify";
 import { IUser } from "../models/user";
+import { INotification } from "../models/notification";
 
 export default class NotificationStore {
     rootStore: RootStore;
@@ -11,5 +12,68 @@ export default class NotificationStore {
     constructor(rootStore: RootStore) {
       this.rootStore = rootStore;
     }
+
+    @observable loadingInitial = false;
+    @observable notificationRegistry = new Map();
+    @observable submitting = false;
+
+    @action loadNotificationsEnviadas = async () => {
+        this.loadingInitial = true;
+        this.notificationRegistry = new Map();
+        try {
+            const notifications = await agent.Notification.listEnviados(
+                parseInt(window.localStorage.getItem("id"))
+                );
+            runInAction("loading notifications", () => {
+                notifications.forEach((notification) => {
+                this.notificationRegistry.set(notification.id, notification);
+                });
+                this.loadingInitial = false;
+            });
+            } catch (error) {
+            runInAction("load notifications error", () => {
+                this.loadingInitial = false;
+            });
+            console.log(error);
+        }
+    };
+
+    
+    @action loadNotificationsRecebidas = async () => {
+        this.loadingInitial = true;
+        this.notificationRegistry = new Map();
+        try {
+            const notifications = await agent.Notification.listRecebidos(
+                parseInt(window.localStorage.getItem("id"))
+                );
+            runInAction("loading notifications", () => {
+                notifications.forEach((notification) => {
+                this.notificationRegistry.set(notification.id, notification);
+                });
+                this.loadingInitial = false;
+            });
+            } catch (error) {
+            runInAction("load notifications error", () => {
+                this.loadingInitial = false;
+            });
+            console.log(error);
+        }
+    };
+
+    @action createNotification = async (notification: INotification) => {
+        this.submitting = true;
+        try {
+          await agent.Notification.create(notification);
+          runInAction("creating notification", () => {
+            this.submitting = false;
+          });
+          history.push("/dashboard");
+        } catch (error) {
+          runInAction("create notification error", () => {
+            this.submitting = false;
+          });
+          throw error;
+        }
+      };
 
 }
